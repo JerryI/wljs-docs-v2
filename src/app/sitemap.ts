de1569dea900b0d9295ba7f4d09192211b7d7354
Kirill Vasin <krikus.ms@gmail.com>
2026-02-23 10:27:02 +0100

    update

---
+++ src/app/sitemap.ts
@@ -0,0 +1,16 @@
+import { source } from '@/lib/source'
+
+export default async function sitemap() {
+  const baseUrl = 'https://wljs.io'
+  const docsUrls = source
+    // Optional: leverage the noIndex property we previously added.
+    // .filter(page => page.data.noIndex !== true)
+    .getPages().map(page => ({
+      url: `${baseUrl}${page.url}`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly' as const,
+      priority: 0.7,
+    }))
+
+  return [...docsUrls]
+}
